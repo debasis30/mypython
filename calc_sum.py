044079f68f4974b6c3fc461a19df52a1cd22e665
debasis30 <72693938+debasis30@users.noreply.github.com>
2020-10-14 10:30:57 +0530

    Update calc_sum.py

--- calc_sum.py
+++ calc_sum.py
@@ -20,6 +20,8 @@ l_sum = 0.0
 l_sum = l_num1 + l_num2
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} is {2}'.format(num1, num2, l_sum))
+print('The sum of {0} and {1} is {2}'.format(num1, num2, l_sum))
+
 
 
