1d701a622d67bfe986f3cba0c6c6696f62cbcf43
debasis30 <72693938+debasis30@users.noreply.github.com>
2020-10-14 10:03:40 +0530

    Add files via upload

---
+++ calc_sum.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+""" Test Program For Demostration 
+    
+    This script add 2 numbers and print the sum.
+
+    ./print_name.py 
+"""
+
+import os
+import sys
+
+import numpy as np
+
+l_num1 = 2.5
+l_num1 = 3.0
+
+# Add two numbers
+sum = l_num1 + l_num1
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
