73573fcc1ae16ca3c92cd1434c291b88b1f5f38b
debasis30 <72693938+debasis30@users.noreply.github.com>
2020-10-26 12:57:07 +0530

    Update calculate_toll.c

--- calculate_toll.c
+++ calculate_toll.c
@@ -14,6 +14,10 @@ int main() {
         printf("%d is even.", num);
     else
         printf("%d is odd.", num);
+     if(num % 2 == 0)
+        printf("%d is even.", num);
+    else
+        printf("%d is odd.", num);
     
     return 0;
 }
